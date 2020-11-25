421930c2946e1795bd7ba5fe28145cb6e2af248b
Arthur Hwang <arthurhwangs@gmail.com>
2020-11-25 20:14:26 +0900

    create event based on avaliable time

--- check_available.py
+++ check_available.py
@@ -3,6 +3,7 @@ from cal_setup import get_google_cal
 from tzlocal import get_localzone
 from list_event import get_list_event
 import create_event 
+from tzlocal import get_localzone
 
 def store_events(count):
     """
@@ -16,6 +17,7 @@ def store_events(count):
     for event in events:
         start = (event['start'].get('dateTime'))
         end = (event['end'].get('dateTime'))
+        title = event['summary']
 
         # Get Time Only (start)
         if start == None:
@@ -34,20 +36,63 @@ def store_events(count):
         if event_vessel == []:
             pass
         else:
-            store_event[f'event{i}'] = str(event_vessel)    
+            store_event[title] = (event_vessel)    
             i += 1 # event number
         event_vessel = [] # reset
 
-        # elapsed = no_isoformat_end - no_isoformat_start
-    return store_event
+    # Sort events in order
+    sorted_values = sorted(store_event.values()) # Sort the values
+    sorted_events = {}
 
+    for i in sorted_values:
+        for k in store_event.keys():
+            if store_event[k] == i:
+                sorted_events[k] = store_event[k]
+                break
+    
+    # Index events
+    index_events = {}
+    for index, (key, (start, end)) in enumerate(sorted_events.items()):
+        index_events[index] = (start, end)
+    return index_events
 
-def check_vacancy(store_events, count):
-    event_days = store_events(count)
+def check_vacancy(duration,count):
+    """
+    Check vacant time block of a given duration.
+    """
+    local_tz = get_localzone() # Call the Local Timezone
+    vacant_times = {}
+    events = store_events(count)
 
+    # List all vacant timeslots and it's duration in order.
+    for i in range(len(events)+1):
+        # from 7:00:00 to the start of first event.
+        if i == 0: 
+            end = events[i][0]
+            start = datetime(end.year, end.month, end.day, 7).astimezone(local_tz)
+        # from end of the last event to 23:00:00.
+        elif i >= len(events):
+            start = events[i-1][1]
+            end = events[i] = datetime(start.year, start.month, start.day, 23).astimezone(local_tz)
+        # inbetween events (end of the one to start of next).
+        else:
+            if events[i-1][1] == events[i][0]:
+                pass
+            else:
+                start = events[i-1][1]
+                end = events[i][0]
+        length = end - start # length of each time block
+        vacant_times[i+1] = (start, end, length)
+
+    # Finds the vacant timeslot from the list of vacant timeslots that works with the duration set by the user.
+    for index, (start,end,length) in vacant_times.items():
+        if vacant_times[index][2] >= timedelta(seconds=(duration+30)*60):
+            return vacant_times[index]
+        else:
+            pass
 
 def main():
-    print(store_events())
+    print(check_vacancy(60,1))
 
 if __name__ == '__main__':
     main()
--- create_event.py
+++ create_event.py
@@ -1,26 +1,38 @@
 from datetime import datetime, timedelta
 from cal_setup import get_google_cal
 from tzlocal import get_localzone
-import list_event
+import check_available
 
-def main():
-    # creates one hour event tomorrow 10 AM IST
-    service = get_google_cal()
-
-    today = datetime.now().date()
-    tomorrow = datetime(today.year, today.month, today.day, 10)+timedelta(days=1)
-    start = tomorrow.isoformat()
-    end = (tomorrow + timedelta(hours=1)).isoformat()
-    title = input("Enter Event Title: ")
-    frequency = input("How many times a week: ")
+def create_calendar(service, title):
+    """
+    Create an calendar with the same name as the new event.
+    """
     local_tz = get_localzone() # Call the Local Timezone
     local_timezone = str(local_tz) # Convert to string
-
+    
     # Calendar Parameter
-    calendar = { 
+    calendar = {
         'summary': title,
         'timeZone': local_timezone
     }
+
+    # Create Calendar
+    created_calendar = service.calendars().insert(body=calendar).execute()
+    return created_calendar['id']
+
+def create_event(service, cal_id, title, frequency, length):
+    """
+    Create event.
+    """
+    local_tz = get_localzone() # Call the Local Timezone
+    local_timezone = str(local_tz) # Convert to string 
+
+    checked = check_available.check_vacancy(int(length),1)
+    available_start = checked[0]
+    available_end = checked[1]
+
+    start = (available_start + timedelta(minutes=15)).isoformat()
+    end = (available_start + timedelta(minutes=(15+int(length)))).isoformat()
 
     # Event Parameter
     event = {
@@ -29,20 +41,29 @@ def main():
         "end": {"dateTime": end, 'timeZone': local_timezone}
     }
     
-    # Create Calendar
-    created_calendar = service.calendars().insert(body=calendar).execute()
     # Create Event
-    create_event = service.events().insert(calendarId=created_calendar['id'], body=event).execute()
-    
+    create_event = service.events().insert(calendarId=cal_id, body=event).execute()   
+
     # Print on Terminal
     print("created calendar")
-    print("calendar id: ", created_calendar['id'])
+    print("calendar id: ", cal_id)
     print("created event")
     print("event id: ", create_event['id'])
     print("summary: ", create_event['summary'])
     print("starts at: ", create_event['start']['dateTime'])
     print("ends at: ", create_event['end']['dateTime'])
     print("calendar link: ", create_event.get('htmlLink'))
+
+
+def main():
+    service = get_google_cal()
+
+    title = input("Enter Event Title: ")
+    frequency = input("How many times a week: ")
+    length = input("How long do you want the event to be? Enter in minutes: ")
+    cal_id = create_calendar(service, title)
+
+    create_event(service, cal_id, title, frequency, length)
 
 if __name__ == '__main__':
     main()
