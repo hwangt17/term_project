fb710cd3241572a041b6991e0c9bc417dc7245c3
Arthur Hwang <arthurhwangs@gmail.com>
2020-11-25 16:25:25 +0900

    check available start

--- check_available.py
+++ check_available.py
@@ -4,8 +4,31 @@ from tzlocal import get_localzone
 from list_event import get_list_event
 import create_event 
 
-def check_vacancy():
-    pass
+def check_vacancy(count):
+    
+    events = get_list_event(count)
+    
+    for event in events:
+        start = str(event['start'].get('dateTime'))
+        end = str(event['end'].get('dateTime'))
 
+        # Get Time Only (start)
+        if start == "None":
+            pass
+        else:
+            no_isoformat_start = datetime.fromisoformat(start)
+        
+        # Get Time Only (end)
+        if end == "None":
+            pass
+        else:
+            no_isoformat_end = datetime.fromisoformat(end)
+        
+        elapsed = no_isoformat_end - no_isoformat_start
+        print(elapsed)
 
-get_list_event(1)
+def main():
+    print(check_vacancy(1))
+
+if __name__ == '__main__':
+    main()
