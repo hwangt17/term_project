5554a0b88f12aefc030430ad16466879e00d5c58
Arthur Hwang <arthurhwangs@gmail.com>
2020-11-26 19:40:25 +0900

    create website

---
+++ fillup_app.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template, request
+from datetime import datetime, timedelta # Datetime
+from cal_setup import get_google_cal # Access Google Acc
+from tzlocal import get_localzone # Get local timezone
+from check_available import vacancy_based_on_freq # Check available timeslot
+from create_event import create_calendar, create_event # Create Calendar and Event
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/index")
+def index():
+    return render_template("index.html")
+
+
+@app.route('/fillup/', methods=['GET','POST'])
+def fillup():
+    service = get_google_cal()
+
+    title = input("Enter Event Title -> ")
+    frequency = input("How many times a week -> ")
+    length = input("How long do you want the event to be? Enter in minutes -> ")
+    earliest_time = input("When do you want the events to start at the earliest? (Enter 0 ~ 23)-> ")
+    latest_time = input("When do you want the events to end at the lastest? (Enter 0 ~ 23) -> ")
+    print("-----------------------")
+    cal_id = create_calendar(service, title)
+
+    vacant = vacancy_based_on_freq(service,int(length),int(frequency),int(earliest_time),int(latest_time))
+    for index, value in vacant.items():
+        available_start = vacant[index][0]
+        start = (available_start + timedelta(minutes=15)).isoformat()
+        end = (available_start + timedelta(minutes=(15+int(length)))).isoformat()
+        create_event(service, cal_id, start, end, title, frequency, length)
+
+    if request.method == "POST":
+        place_name = str(request.form['place'])
+        mbta_station = find_stop_near(place_name)
+        station = mbta_station[0]
+        wheelchair_accessible = mbta_station[1]
+
+        if mbta_station:
+            return render_template(
+                "result.html",
+                place_name = place_name,
+                station = station,
+                wheelchair_accessible = wheelchair_accessible
+            )
+        else:
+            return render_template("automate.html", error = True)
+
+    return render_template("automate.html", error = None)
